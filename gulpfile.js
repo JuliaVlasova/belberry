a3d2c93ade1135c2a8a96c23a4cb7992c48726a1
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-09-16 19:41:44 +0300

    html modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,47 @@
-const { src, dest } = require('gulp');
-const gulpif = require('gulp-if');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const minifyCSS = require('gulp-minify-css');
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest } = require("gulp");
+const { series } = require("gulp");
+const gulpif = require("gulp-if");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const minifyCSS = require("gulp-minify-css");
+const sass = require("gulp-sass")(require("sass"));
+const fileinclude = require("gulp-file-include");
 
 function isJavaScript(file) {
-    return file.extname === '.js';
+  return file.extname === ".js";
 }
 
 function isScss(file) {
-    return file.extname === '.scss';
+  return file.extname === ".scss";
 }
 
 function isCss(file) {
-    return file.extname === '.css';
+  return file.extname === ".css";
 }
 
-exports.default = function() {
-    return src(['src/js/*.js', 'src/style/*.scss'])
+function defaultTask(cb) {
+  return src(["src/js/*.js", "src/style/*.scss"])
     .pipe(gulpif(isJavaScript, uglify()))
     .pipe(gulpif(isScss, sass()))
     .pipe(gulpif(isCss, minifyCSS()))
-    .pipe(gulpif(isJavaScript, rename({ extname: '.min.js' })))
-    .pipe(gulpif(isCss, rename({ extname: '.min.css' })))
-    .pipe(gulpif(isCss, dest('output/css/')))
-    .pipe(gulpif(isJavaScript, dest('output/js/')));
+    .pipe(gulpif(isJavaScript, rename({ extname: ".min.js" })))
+    .pipe(gulpif(isCss, rename({ extname: ".min.css" })))
+    .pipe(gulpif(isCss, dest("output/css/")))
+    .pipe(gulpif(isJavaScript, dest("output/js/")));
+  cb();
 }
 
+function build(cb) {
+    return src(["src/templates/index.html"])
+      .pipe(
+        fileinclude({
+          prefix: "@@",
+          basepath: "@file",
+        })
+      )
+      .pipe(dest("./"));
+  cb();
+}
+
+exports.build = build;
+exports.default = series(defaultTask, build);
