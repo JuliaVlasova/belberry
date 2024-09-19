7d6e22b238e9662f06435fe5af0f92956f70b25c
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-09-19 17:42:22 +0300

    section 7

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,10 +1,34 @@
 "use strict";
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".p-slider .swiper", {
   direction: "horizontal",
   loop: true,
 
   pagination: {
     el: ".swiper-pagination",
+  },
+});
+
+const swiperDoctors = new Swiper(".p-doctors-accordion .swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  keyboard: true,
+  breakpoints: {
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
   },
 });
