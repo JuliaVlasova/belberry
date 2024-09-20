583c2552bd4c132e2b033ca9cedef0cdce7237be
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-09-20 12:53:53 +0300

    section 11

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,15 +3,22 @@
 const swiper = new Swiper(".p-slider .swiper", {
   direction: "horizontal",
   loop: true,
-
+  autoplay: {
+    delay: 1000,
+  },
   pagination: {
     el: ".swiper-pagination",
+    type: "bullets",
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '<i></i>' + '<b></b>'  + '</span>';
+    },
   },
 });
 
 const swiperDoctors = new Swiper(".p-doctors-accordion .swiper", {
   slidesPerView: 1,
-  spaceBetween: 10,
+  spaceBetween: 20,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -24,11 +31,61 @@ const swiperDoctors = new Swiper(".p-doctors-accordion .swiper", {
     },
     "@1.00": {
       slidesPerView: 3,
-      spaceBetween: 40,
+      spaceBetween: 20,
     },
     "@1.50": {
       slidesPerView: 4,
-      spaceBetween: 50,
+      spaceBetween: 20,
+    },
+  },
+});
+
+const swiperTestimonials = new Swiper(".p-testimonials__slider .swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  keyboard: true,
+  breakpoints: {
+    "@0.75": {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.50": {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+
+const swiperPress = new Swiper(".p-press .swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  keyboard: true,
+  breakpoints: {
+    "@0.75": {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.50": {
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
   },
 });
