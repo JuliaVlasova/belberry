4835d7b345bcaa8cba864b41b593cdafc603149e
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-09-23 17:21:57 +0300

    mobile menu and first screen

--- src/js/go-to-top.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$(document).ready(function() {
-    $(".beautiful-scroll").each(function() {
-        $(this).click(function() {
-            $("html, body").animate({scrollTop:0}, "slow");
-        });
-    });
-});
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,26 +1,9 @@
 "use strict";
 
-let showMobileMenu = function () {
-	$(".nav-buttons").addClass("nav-buttons_opened");
-    $(".nav-mobile-menu-close").addClass("nav-mobile-menu-close_opened");
-};
-
-let hideMobileMenu = function () {
-	$(".nav-buttons").removeClass("nav-buttons_opened");
-    $(".nav-mobile-menu-close").removeClass("nav-mobile-menu-close_opened");
-};
-
 $(document).ready(function() {
     $(".nav-mobile-burger").click(function() {
-        showMobileMenu();
-    });
-
-    $(".nav-buttons_close-mobile").click(function() {
-        hideMobileMenu();
-    });
-
-    $(".nav-buttons__link").click(function() {
-        hideMobileMenu();
-        $(".block-with-button__image").removeClass("block-with-button__image_animated");
+        $(".nav-buttons").toggleClass("nav-buttons_opened");
+        $(".nav-mobile-burger").toggleClass("nav-close");
+        $(".nav").toggleClass("nav_fixed");
     });
 });
