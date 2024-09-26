8b1dc12e9aad58a63b21dbec6eceb857ecd2616d
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-09-26 09:54:23 +0300

    mobile version ready

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -6,4 +6,10 @@ $(document).ready(function() {
         $(".nav-mobile-burger").toggleClass("nav-close");
         $(".nav").toggleClass("nav_fixed");
     });
+
+    $(".nav__button").click(function() {
+        $(".nav-buttons").removeClass("nav-buttons_opened");
+        $(".nav-mobile-burger").removeClass("nav-close");
+        $(".nav").removeClass("nav_fixed");
+    });
 });
